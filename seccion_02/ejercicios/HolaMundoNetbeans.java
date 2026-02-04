86e12c2a5ba61acd0e2d19e08c9c59d518ab4a0e
diegofdg <diegofdg@gmail.com>
2026-02-04 05:21:26 -0300

    seccion 2-hola mundo con netbeans

---
+++ seccion_02/ejercicios/HolaMundoNetbeans.java
@@ -0,0 +1,6 @@
+public class HolaMundoNetbeans {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo con Netbeans");
+        System.out.println("Saludos");
+    }
+}
