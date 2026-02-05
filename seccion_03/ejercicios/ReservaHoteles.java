ffb8247f44c04774b82881e3fc0244141c7ebdab
diegofdg <diegofdg@gmail.com>
2026-02-05 08:06:12 -0300

    seccion 3-variables en java

---
+++ seccion_03/ejercicios/ReservaHoteles.java
@@ -0,0 +1,31 @@
+public class ReservaHoteles {
+    public static void main(String[] args) {
+        System.out.println("*** Sistema de Reserva de Hoteles ***");
+
+        // Definimos las variables
+        var nombreCliente = "Miguel Flores";
+        var diasEstancia = 7;
+        var tarifaDiaria = 1300.00;
+        var tieneVistaAlMar = true;
+
+        // Mostrar el detalle de la reserva
+        System.out.println("nombreCliente = " + nombreCliente);
+        System.out.println("diasEstancia = " + diasEstancia);
+        System.out.println("tarifaDiaria = " + tarifaDiaria);
+        System.out.println("tieneVistaAlMar = " + tieneVistaAlMar);
+
+        // Modificamos algunos valores
+        diasEstancia = 5;
+        tarifaDiaria = 900.00;
+        tieneVistaAlMar = false;
+
+        // Mostrar el detalle de la reserva
+        System.out.println();
+        System.out.println("Nuevos datos de la reservación: ");
+        System.out.println("nombreCliente = " + nombreCliente);
+        System.out.println("diasEstancia = " + diasEstancia);
+        System.out.println("tarifaDiaria = " + tarifaDiaria);
+        System.out.println("tieneVistaAlMar = " + tieneVistaAlMar);
+
+    }
+}
