060a0c23ca3186d7800369eaf73e103c67ea9b73
diegofdg <diegofdg@gmail.com>
2026-03-01 07:45:45 -0300

    seccion 31-servlets y jsps-introduccion al curso

---
+++ seccion_31/ejercicios/HolaMundoServlets/src/main/java/gm/com/mx/HolaMundo.java
@@ -0,0 +1,17 @@
+package gm.com.mx;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/HolaMundoServlet") //http:8080/HolaMundoJavaWeb/HolaMundoServlet
+public class HolaMundo extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        var salida = response.getWriter();
+        salida.print("Hola Mundo desde Servlets");
+    }
+}
