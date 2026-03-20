a16b2f2ecde5795e218da0c182ab3787de4f6f89
diegofdg <diegofdg@gmail.com>
2026-03-20 07:25:42 -0300

    seccion 50-javaserver faces-ciclo de vida de jsf

---
+++ seccion_50/ejercicios/CicloVidaJSF/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,26 @@
+package beans.model;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Named("candidato")
+@SessionScoped
+public class Candidato implements Serializable{
+    
+    private static final Logger log = LoggerFactory.getLogger(Candidato.class);
+    private String nombre = "Valor";
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        log.info("Actualizando nombre: " + nombre);
+        this.nombre = nombre;
+    }
+    
+    
+}
