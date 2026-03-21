24a62501a92c980e9344698762305123c9ce669f
diegofdg <diegofdg@gmail.com>
2026-03-21 07:28:27 -0300

    seccion 51-javaserver faces-validadores en jsf

---
+++ seccion_51/ejercicios/ValidadoresJSF/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,53 @@
+package beans.model;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Named("candidato")
+@SessionScoped
+public class Candidato implements Serializable{
+    
+    private static final Logger log = LoggerFactory.getLogger(Candidato.class);
+    private String nombre;
+    private String apellido;
+    private String salarioDeseado;
+    
+    public Candidato(){
+        log.info("Creando el objeto Candidato");
+        this.nombre = "Introduce tu nombre";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        log.info("Actualizando nombre: " + nombre);
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        log.info("Actualizando apellido: " + apellido);
+        this.apellido = apellido;
+    }
+
+    public String getSalarioDeseado() {
+        return salarioDeseado;
+    }
+
+    public void setSalarioDeseado(String salarioDeseado) {
+        log.info("Actuaizando salario: " + salarioDeseado);
+        this.salarioDeseado = salarioDeseado;
+    }
+    
+    
+    
+    
+}
