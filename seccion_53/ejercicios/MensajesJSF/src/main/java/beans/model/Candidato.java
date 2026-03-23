4f65f9da0f320e2d3ab2f42116d839bb348d3638
diegofdg <diegofdg@gmail.com>
2026-03-23 06:21:21 -0300

    seccion 53-javaserver faces-mensajes e internacionalizacion en jsf

---
+++ seccion_53/ejercicios/MensajesJSF/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,61 @@
+package beans.model;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Named("candidato")
+@SessionScoped
+public class Candidato implements Serializable {
+
+    private static final Logger log = LoggerFactory.getLogger(Candidato.class);
+
+    private String nombre;
+    private String apellido;
+    private int salarioDeseado;
+    private Date fechaNacimiento;
+
+    public Candidato() {
+        log.info("Creando el objeto Candidato");
+        this.nombre = "Introduce tu nombre";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        log.info("Actualizando nombre: " + nombre);
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        log.info("Actualizando apellido: " + apellido);
+        this.apellido = apellido;
+    }
+
+    public int getSalarioDeseado() {
+        return salarioDeseado;
+    }
+
+    public void setSalarioDeseado(int salarioDeseado) {
+        log.info("Actuaizando salario: " + salarioDeseado);
+        this.salarioDeseado = salarioDeseado;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+}
