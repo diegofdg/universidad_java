f4ba8e1ae56545d1319b6bf16eb29844e34bf047
diegofdg <diegofdg@gmail.com>
2026-03-30 19:03:47 -0300

    seccion 60-jakarta ee-java persistente api (jpa)

---
+++ seccion_60/ejercicios/LaboratorioSgaWeb-Usuario/sga-jee-web/src/main/java/sga/web/UsuarioServlet.java
@@ -0,0 +1,27 @@
+package sga.web;
+
+import java.io.IOException;
+import java.util.List;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import sga.domain.Usuario;
+import sga.servicio.UsuarioService;
+
+@WebServlet("/usuarios")
+public class UsuarioServlet extends HttpServlet{
+    
+    @Inject
+    UsuarioService usuarioService;
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        List<Usuario> usuarios = usuarioService.listarUsuarios();
+        System.out.println("usuarios:" + usuarios);
+        request.setAttribute("usuarios", usuarios);
+        request.getRequestDispatcher("/listadoUsuarios.jsp").forward(request, response);
+    }
+}
