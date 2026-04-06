9be3fbdda9b3d087bd30be53c42ca5e17d3bbddc
diegofdg <diegofdg@gmail.com>
2026-04-06 19:56:17 -0300

    seccion 67-jakarta ee-web service con jax-ws en jakarta ee

---
+++ seccion_67/ejercicios/JAX-WS-Cliente/ClienteSumaWS/src/main/java/clientews/servicio/package-info.java
@@ -0,0 +1,2 @@
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://beans/")
+package clientews.servicio;
---
+++ seccion_67/ejercicios/Sga-Jax-ws-Servicio/sga-jee-web/src/main/java/sga/servicio/PersonaServiceImpl.java
@@ -0,0 +1,47 @@
+package sga.servicio;
+
+import java.util.List;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.jws.WebService;
+import sga.datos.PersonaDao;
+import sga.domain.Persona;
+
+@Stateless
+@WebService(endpointInterface = "sga.servicio.PersonaServiceWs")
+public class PersonaServiceImpl implements PersonaServiceRemote, PersonaService, PersonaServiceWs{
+
+    @Inject
+    private PersonaDao personaDao;
+    
+    @Override
+    public List<Persona> listarPersonas() {
+       return personaDao.findAllPersonas();
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return personaDao.findPersonaById(persona);
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return personaDao.findPersonaByEmail(persona);
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+        personaDao.insertPersona(persona);
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+        personaDao.updatePersona(persona);
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+        personaDao.deletePersona(persona);
+    }
+    
+}
