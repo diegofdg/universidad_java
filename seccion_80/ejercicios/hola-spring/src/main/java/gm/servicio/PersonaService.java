0145d62bba54a42b9dda065aece92fb370cfef08
diegofdg <diegofdg@gmail.com>
2026-04-19 08:35:54 -0300

    seccion 80-spring-manejo transaccional

---
+++ seccion_80/ejercicios/hola-spring/src/main/java/gm/servicio/PersonaService.java
@@ -0,0 +1,15 @@
+package gm.servicio;
+
+import java.util.List;
+import gm.domain.Persona;
+
+public interface PersonaService {
+    
+    public List<Persona> listarPersonas();
+    
+    public void guardar(Persona persona);
+    
+    public void eliminar(Persona persona);
+    
+    public Persona encontrarPersona(Persona persona);
+}
